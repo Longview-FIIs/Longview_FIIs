6ed0ffa5e315e0fde50ee56b0d6ad0f396d6076e
guilhermemm9 <130560970+guilhermemm9@users.noreply.github.com>
2025-07-24 16:13:34 -0300

    Import Streamlit

--- Ferramenta/variaveis.py
+++ Ferramenta/variaveis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import datetime
+import streamlit as st
 
 # Caminho para seu arquivo Excel
 arquivo = r"C:\Users\User\Documents\OneDrive\Documentos\Guilherme\Precos_Reuters.xlsm"
